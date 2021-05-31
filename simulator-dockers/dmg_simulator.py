613e60c3c7e57718473458616e140697cac52cb2
Voigt, Michael <Michael.Voigt@softwareag.com>
2021-05-31 12:59:47 +0200

    COE-16: update backend config & add more logging

--- simulator-dockers/dmg_simulator.py
+++ simulator-dockers/dmg_simulator.py
@@ -12,7 +12,7 @@ logging.info(os.environ)
 '''
 Start configuration
 '''
-C8Y_BASE = 'http://hackathon.adamos-dev.com' #os.environ.get('C8Y_BASEURL')
+C8Y_BASE = 'http://mobile.eu-latest.cumulocity.com' #os.environ.get('C8Y_BASEURL')
 C8Y_TENANT = os.environ.get('C8Y_TENANT')
 C8Y_USER = os.environ.get('C8Y_USER')
 C8Y_PASSWORD = os.environ.get('C8Y_PASSWORD')
@@ -27,11 +27,15 @@ logging.info(C8Y_TENANT)
 logging.info(C8Y_USER)
 logging.info(C8Y_PASSWORD)
 
+logging.info("Starting DMG Simluator ...")
+
 C8Y_HEADERS = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
     'Authorization': 'Basic ' + base64.b64encode(C8Y_TENANT + '/' + C8Y_USER + ':' + C8Y_PASSWORD)
 }
+
+logging.info("Starting DMG Simluator 1...")
 
 def sendMeasurement(measurement):
     response = requests.post(C8Y_BASE + '/measurement/measurements', headers=C8Y_HEADERS, data=json.dumps(measurement))
@@ -52,24 +56,31 @@ def sendEvent(event):
     response = requests.post(C8Y_BASE + '/event/events', headers=C8Y_HEADERS, data=json.dumps(event))
     return response.json()
 
+logging.info("Starting DMG Simluator 2...")
+
 # Check if device already created
 response = requests.get(C8Y_BASE + '/identity/externalIds/c8y_SimulatorSerial/' + C8Y_SIMULATOR_EXTERNAL_ID, headers=C8Y_HEADERS)
+logging.info("response: " + response)
 
 if (response.status_code == 200):
     deviceId = response.json()['managedObject']['id']
+    logging.info("Docker Sim device exists: " + deviceId)
 else:
     # Create device
+    logging.info("create device...")
     device = {
         'name': 'Docker DMG Mori Simulator Device',
         'c8y_IsDevice': {}
     }
     response = requests.post(C8Y_BASE + '/inventory/managedObjects', headers=C8Y_HEADERS, data=json.dumps(device))
     deviceId = response.json()['id']
+    logging.info(deviceId)
     externalId = {
         'type': 'c8y_SimulatorSerial',
         'externalId': C8Y_SIMULATOR_EXTERNAL_ID
     }
     response = requests.post(C8Y_BASE + '/identity/globalIds/' + deviceId + '/externalIds', headers=C8Y_HEADERS, data=json.dumps(externalId))
+    logging.info(response)
 
 logging.info('Device ID: ' + deviceId)
 
