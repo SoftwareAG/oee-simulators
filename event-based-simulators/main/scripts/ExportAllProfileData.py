fe0235ce4a4e56a2e13d9f5167bd9c996f038104
Nam Nhat Pham <naph@softwareag.com>
2022-11-03 16:48:54 +0100

    input device name to search in device management list

--- event-based-simulators/main/scripts/ExportAllProfileData.py
+++ event-based-simulators/main/scripts/ExportAllProfileData.py
@@ -1,35 +1,43 @@
 import ArgumentsAndCredentialsHandler
+import Credentials
 
 
-class exportAllProfileData:
-    def exportAllProfileData(c8y):
-        # Loop through the list of device in Device management
-        for device in c8y.device_inventory.select():
-            print(f"Found device '{device.name}', id: #{device.id}, "
-                  f"owned by {device.owner}, number of children: {len(device.child_devices)}")
-            # Check each child device of the main device with device.id
-            for childDevice in device.child_devices:
-                print(f"Child devices #{childDevice.id}")
-                # listing alarms of child device
-                print(f"Begin LISTING ALARMS of device {childDevice.name}, id #{childDevice.id}")
-                for alarm in c8y.alarms.select(source=childDevice.id):
-                    print(f"Found alarm #{alarm.id}, severity: {alarm.severity}\n")
-                    print(f"{alarm.text}\n")
-                    print(f"fragments: {list(alarm.keys())}\n")
-                # listing measurements of child device
-                for measurement in c8y.measurements.select(source=childDevice.id):
-                    print(f"Found measurement id #{measurement.id}\n, type: {measurement.type}\n")
-                    for measurementKey in measurement.fragments:
-                        print(f"{measurementKey}: {measurement.fragments.get(measurementKey)}")
+def exportAllProfileData(c8y):
+    # Loop through the list of device in Device management
+    for device in c8y.device_inventory.select(type="c8y_EventBasedSimulator"):
+        print(f"Found device '{device.name}', id: #{device.id}, "
+                f"owned by {device.owner}, number of children: {len(device.child_devices)}")
+        # Check each child device of the main device with device.id
+        for childDevice in device.child_devices:
+            print(f"Child devices #{childDevice.id}")
+            # listing alarms of child device
+            print(f"Begin LISTING ALARMS of device {childDevice.name}, id #{childDevice.id}")
+            for alarm in c8y.alarms.select(source=childDevice.id):
+                print(f"Found alarm #{alarm.id}, severity: {alarm.severity}\n")
+                print(f"{alarm.text}\n")
+                print(f"fragments: {list(alarm.keys())}\n")
+            # listing measurements of child device
+            for measurement in c8y.measurements.select(source=childDevice.id):
+                print(f"Found measurement id #{measurement.id}\n, type: {measurement.type}\n")
+                for measurementKey in measurement.fragments:
+                    print(f"{measurementKey}: {measurement.fragments.get(measurementKey)}")
 
 
 # Main function to run the script
 if __name__ == '__main__':
-    args = ArgumentsAndCredentialsHandler.argumentsParser()
-    C8Y_BASE = args.baseurl
-    C8Y_TENANT = args.id
-    C8Y_USER = args.username
-    C8Y_PASSWORD = args.password
-    MODE = args.mode
-    print(f"{C8Y_PASSWORD}, {C8Y_USER}, {C8Y_BASE}, {C8Y_TENANT}, {MODE}")
-    print(f"FINISH PRINTING")
+    try:
+        args = ArgumentsAndCredentialsHandler.argumentsParser()
+        C8Y_BASE = args.baseurl
+        C8Y_TENANT = args.id
+        C8Y_USER = args.username
+        C8Y_PASSWORD = args.password
+        MODE = args.mode
+    except:
+        C8Y_BASE = Credentials.C8Y_BASE
+        C8Y_TENANT = Credentials.C8Y_TENANT
+        C8Y_USER = Credentials.C8Y_USER
+        C8Y_PASSWORD = Credentials.C8Y_PASSWORD
+        
+    print(f"{C8Y_PASSWORD}, {C8Y_USER}, {C8Y_BASE}, {C8Y_TENANT}")
+    c8y = ArgumentsAndCredentialsHandler.c8yPlatformConnection(C8Y_BASE, C8Y_TENANT, C8Y_USER, C8Y_PASSWORD)
+    exportAllProfileData(c8y)
--- event-based-simulators/main/scripts/ExportSpecificProfileData.py
+++ event-based-simulators/main/scripts/ExportSpecificProfileData.py
@@ -2,12 +2,19 @@ import ArgumentsAndCredentialsHandler
 import Credentials
 
 
-def exportSpecificProfileData(c8y):
-    for device in c8y.device_inventory.select(type="c8y_EventBasedSimulator"):
+def exportSpecificProfileData(c8y, deviceName):
+    deviceCount = 0
+    deviceIntentorySelect = c8y.device_inventory.select(type="c8y_EventBasedSimulator", name=deviceName)
+
+    for device in deviceIntentorySelect:
         print(f"Found device '{device.name}', id: #{device.id}, "
               f"owned by {device.owner}, number of children: {len(device.child_devices)}, type: {device.type}")
+        deviceCount += 1
         for childDevice in device.child_devices:
             print(f"Child device {childDevice.name}, id #{childDevice.id}, owned by device {device.name}")
+
+    if deviceCount == 0:
+        print(f"No device with name {deviceName} found")
 
 
 # Main function to run the script
@@ -24,7 +31,9 @@ if __name__ == '__main__':
         C8Y_TENANT = Credentials.C8Y_TENANT
         C8Y_USER = Credentials.C8Y_USER
         C8Y_PASSWORD = Credentials.C8Y_PASSWORD
-
-    print(f"{C8Y_PASSWORD}, {C8Y_USER}, {C8Y_BASE}, {C8Y_TENANT}")
+        
+    print('Enter device name to search: ')
+    deviceName = input()
+    print(f"{C8Y_PASSWORD}, {C8Y_USER}, {C8Y_BASE}, {C8Y_TENANT}, {deviceName}")
     c8y = ArgumentsAndCredentialsHandler.c8yPlatformConnection(C8Y_BASE, C8Y_TENANT, C8Y_USER, C8Y_PASSWORD)
-    exportSpecificProfileData(c8y)
+    exportSpecificProfileData(c8y, deviceName)
