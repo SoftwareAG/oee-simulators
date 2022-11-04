e50a1972f661fcf9dac0d066a48d8395e4e041e0
Nam Nhat Pham <naph@softwareag.com>
2022-11-04 10:45:55 +0100

    improve the credential and connection handle

--- event-based-simulators/main/scripts/ArgumentsAndCredentialsHandler.py
+++ event-based-simulators/main/scripts/ArgumentsAndCredentialsHandler.py
@@ -1,21 +1,19 @@
-# importing the requests library
 import argparse
-from datetime import datetime, timezone
+import Credentials
+
 from c8y_api import CumulocityApi
 
 
 def argumentsParser():
     parser = argparse.ArgumentParser(description='Script to export all profiles data')
     parser.add_argument('--baseurl', '-b', type=str, help='Base URL of the tenant', required=True)
-    parser.add_argument('--id', '-i', type=str, help='Tenant ID', required=True)
+    parser.add_argument('--id-tenant', '-i', type=str, help='Tenant ID', required=True)
     parser.add_argument('--username', '-u', type=str, help='Tenant\'s username', required=True)
     parser.add_argument('--password', '-p', type=str, help='Tenant\'s password', required=True)
-    parser.add_argument('--all', '-a', action='store_true', help='print all profiles option')
     parser.add_argument('--mode', '-m', choices=['measurements', 'alarms']
                         , help='Choose mode to extract data.\n "measurement" to '
                                'extract measurement,\n "alarm" to '
-                               'extract alarm'
-                        , required=True)
+                               'extract alarm')
 
     # Check if arguments are input right
     try:
@@ -26,12 +24,24 @@ def argumentsParser():
         raise
 
 
-def c8yPlatformConnection(C8Y_BASE, C8Y_TENANT, C8Y_USER, C8Y_PASSWORD):
+def c8yPlatformConnection():
+    try:
+        args = argumentsParser()
+        C8Y_BASE = args.baseurl
+        C8Y_TENANT = args.id_tenant
+        C8Y_USER = args.username
+        C8Y_PASSWORD = args.password
+        MODE = args.mode
+    except:
+        C8Y_BASE = Credentials.C8Y_BASE
+        C8Y_TENANT = Credentials.C8Y_TENANT
+        C8Y_USER = Credentials.C8Y_USER
+        C8Y_PASSWORD = Credentials.C8Y_PASSWORD
+
+    print(f"{C8Y_PASSWORD}, {C8Y_USER}, {C8Y_BASE}, {C8Y_TENANT}, {MODE}")
     c8y = CumulocityApi(base_url=C8Y_BASE,  # the url of your Cumulocity tenant here
                         tenant_id=C8Y_TENANT,  # the tenant ID of your Cumulocity tenant here
                         username=C8Y_USER,  # your Cumulocity IoT username
                         password=C8Y_PASSWORD)  # your Cumulocity IoT password
-    return c8y
 
-
-
+    return c8y, MODE
