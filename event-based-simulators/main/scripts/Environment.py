85e0b2e8012a71db373344fe326b0d637dc556a2
Nam Nhat Pham <naph@softwareag.com>
2022-11-08 16:24:03 +0100

    Sample environment file

--- event-based-simulators/main/scripts/Environment.py
+++ event-based-simulators/main/scripts/Environment.py
@@ -1,4 +1,11 @@
+# Required
 C8Y_BASE = 'http://localhost:8080'
 C8Y_TENANT = 't100'
 C8Y_USER = 'test'
 C8Y_PASSWORD = 'test'
+
+# Optional
+ACTION = 'export'  # 'import': upload json file in export_data to c8y, 'export': export data from c8y to json file
+MODE = 'specific'  # 'all': export all measurements or alarms data from c8y, 'specific': export only data from chosen device
+DATA_TYPE = 'alarms'  # 'alarms': export alarms data, 'measurements': export measurements data
+DEVICE_NAME = 'Normal #1'  # Choose a device name in device management to extract data in 'specific' mode
