969dba3b424ba5e265fb688f3068424508d70cc0
Nam Nhat Pham <naph@softwareag.com>
2022-12-21 13:43:33 +0100

    COE-796: Adapt change from simulators.json to event simulator

--- simulators/main/simulators.py
+++ simulators/main/simulators.py
@@ -110,8 +110,8 @@ class MachineSimulator:
                 return frequency / 3600.0
             if event_type == "Pieces_Produced":
                 frequency = event_definition.get("frequency")
-                countMaximumFrequency = event_definition.get("countMaximumFrequency")
-                return frequency * countMaximumFrequency / 3600.0
+                countMaximumPerHour = event_definition.get("countMaximumPerHour")
+                return frequency * countMaximumPerHour / 3600.0
 
         return 0.0
 
@@ -213,12 +213,12 @@ class MachineSimulator:
 
         self.__produce_pieces()
 
-        countMaximumFrequency = event_definition.get("countMaximumFrequency") or 10
+        countMaximumPerHour = event_definition.get("countMaximumPerHour") or 10
 
         frequency = event_definition.get("frequency") or 1
 
         event = self.__type_fragment(event_definition)
-        pieces_produced = self.__pick_pieces(frequency * countMaximumFrequency / 3600.0)
+        pieces_produced = self.__pick_pieces(frequency * countMaximumPerHour / 3600.0)
         event.update({"count": pieces_produced})
         event.update(self.__get_production_info())
 
@@ -239,15 +239,15 @@ class MachineSimulator:
     def __on_pieces_ok_event(self, event_definition, task):
         event = self.__type_fragment(event_definition)
 
-        countMinimumFrequency = event_definition.get("countMinimumFrequency") or 0
-        countMaximumFrequency = event_definition.get("countMaximumFrequency") or 10
+        countMinimumPerHour = event_definition.get("countMinimumPerHour") or 0
+        countMaximumPerHour = event_definition.get("countMaximumPerHour") or 10
 
         piece_produced_timestamp = None
         if hasattr(task, 'extra'):
             piece_produced_timestamp = task.extra["timestamp"]
-            countMaximumFrequency = task.extra.get("pieces_produced") or countMaximumFrequency
+            countMaximumPerHour = task.extra.get("pieces_produced") or countMaximumPerHour
 
-        event.update({"count": randint(min(countMinimumFrequency, countMaximumFrequency), countMaximumFrequency)})
+        event.update({"count": randint(min(countMinimumPerHour, countMaximumPerHour), countMaximumPerHour)})
 
         self.__send_event(event, piece_produced_timestamp)
 
