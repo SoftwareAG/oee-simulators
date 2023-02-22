c944acba927707d67585e3892a8845c57d3e902a
Nam Nhat Pham <naph@softwareag.com>
2023-02-22 16:11:46 +0100

    COE-810: add test to create and activate oee profile

--- test/simulators_test.py
+++ test/simulators_test.py
@@ -1,27 +1,25 @@
 import unittest, logging, os, sys
 import config.root # Set source directory
 
-from simulators.main.oeeAPI import ProfileCreateMode, OeeAPI
+from simulators.main.oeeAPI import ProfileCreateMode, OeeAPI, substitute
 from simulators.main.simulator import get_or_create_device_id, load
-from simulators.main.cumulocityAPI import CumulocityAPI
+from simulators.main.cumulocityAPI import CumulocityAPI, C8Y_TENANT
 from unittest.mock import patch
-
 
 log = logging.getLogger("Test")
 logging.basicConfig(format='%(asctime)s %(name)s:%(message)s', level=logging.DEBUG)
 
-cumulocityAPI = CumulocityAPI()
-oeeAPI = OeeAPI()
-
-# Get Tenant Options and configure Simulator
-MICROSERVICE_OPTIONS = cumulocityAPI.get_tenant_option_by_category("simulators")
-PROFILE_CREATE_MODE = ProfileCreateMode[MICROSERVICE_OPTIONS.get("CREATE_PROFILES", "CREATE_IF_NOT_EXISTS")]
-CREATE_PROFILES_ARGUMENTS = MICROSERVICE_OPTIONS.get("CREATE_PROFILES_ARGUMENTS", "")
-CREATE_ASSET_HIERARCHY = MICROSERVICE_OPTIONS.get("CREATE_ASSET_HIERARCHY", "False")
-DELETE_PROFILES = MICROSERVICE_OPTIONS.get("DELETE_PROFILES", "False")
-
 
 class Test(unittest.TestCase):
+    def setUp(self):
+        self.oee_api = OeeAPI()
+        self.cumulocity_api = CumulocityAPI()
+        # Get Tenant Options and configure Simulator
+        self.MICROSERVICE_OPTIONS = self.cumulocity_api.get_tenant_option_by_category("simulators")
+        self.PROFILE_CREATE_MODE = ProfileCreateMode[self.MICROSERVICE_OPTIONS.get("CREATE_PROFILES", "CREATE_IF_NOT_EXISTS")]
+        self.CREATE_PROFILES_ARGUMENTS = self.MICROSERVICE_OPTIONS.get("CREATE_PROFILES_ARGUMENTS", "")
+        self.CREATE_ASSET_HIERARCHY = self.MICROSERVICE_OPTIONS.get("CREATE_ASSET_HIERARCHY", "False")
+        self.DELETE_PROFILES = self.MICROSERVICE_OPTIONS.get("DELETE_PROFILES", "False")
 
     def test_get_or_create_device_id_with_full_model_and_delete(self):
         self.device_model = {
@@ -35,7 +33,7 @@ class Test(unittest.TestCase):
         self.device_id = get_or_create_device_id(device_model=self.device_model)
         # null device_id will fail the test
         self.assertIsNotNone(self.device_id)
-        cumulocityAPI.delete_managed_object(self.device_id)
+        self.cumulocity_api.delete_managed_object(self.device_id)
         log.info(f"Removed the test device with id {self.device_id}")
         log.info('-' * 100)
 
@@ -72,6 +70,23 @@ class Test(unittest.TestCase):
         if not self.model:
             self.model = load("../simulators/main/simulator.json") # Load model for unittest on IDE
         self.assertIsNotNone(self.model)
+        log.info('-' * 100)
+
+    def test_create_and_activate_oee_profile(self):
+        self.device_model = {
+            "type": "Simulator",
+            "id": "sim_001_test",
+            "label": "Test Simulator #1",
+            "enabled": "true"
+        }
+        self.device_id = get_or_create_device_id(device_model=self.device_model)
+        self.device_profile_info = self.oee_api.create_and_activate_profile(external_id=self.device_model.get('id'))
+        # null device_id will fail the test
+        self.assertIsNotNone(self.device_profile_info)
+        self.cumulocity_api.delete_managed_object(self.device_profile_info.get('id'))
+        log.info(f"Removed the test oee profile with id {self.device_profile_info.get('id')}")
+        self.cumulocity_api.delete_managed_object(self.device_id)
+        log.info(f"Removed the test device with id {self.device_id}")
         log.info('-' * 100)
 
 
