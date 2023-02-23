a595a41371c056c1034f769ba09c3a65b32eed34
Nam Nhat Pham <naph@softwareag.com>
2023-02-23 10:00:01 +0100

    COE-810: create class Utils and method create_device

--- test/simulators_test.py
+++ test/simulators_test.py
@@ -20,74 +20,71 @@ class Test(unittest.TestCase):
         self.CREATE_PROFILES_ARGUMENTS = self.MICROSERVICE_OPTIONS.get("CREATE_PROFILES_ARGUMENTS", "")
         self.CREATE_ASSET_HIERARCHY = self.MICROSERVICE_OPTIONS.get("CREATE_ASSET_HIERARCHY", "False")
         self.DELETE_PROFILES = self.MICROSERVICE_OPTIONS.get("DELETE_PROFILES", "False")
-
-    def test_get_or_create_device_id_with_full_model_and_delete(self):
         self.device_model = {
             "type": "Simulator",
             "id": "sim_001_test",
             "label": "Test Simulator #1",
             "enabled": "true"
         }
-        log.info(f"Created device model: {self.device_model}")
-        log.info("Start testing create device and adding external id")
-        self.device_id = get_or_create_device_id(device_model=self.device_model)
+        self.device_model_no_id = {
+            "type": "Simulator",
+            "label": "Test Simulator #1",
+            "enabled": "true"
+        }
+        self.device_model_no_label = {
+            "type": "Simulator",
+            "id": "sim_001_test",
+            "enabled": "true"
+        }
+
+    def test_get_or_create_device_id_with_full_model_and_delete(self):
+        device_id = Utils.create_device(self.device_model)
         # null device_id will fail the test
-        self.assertIsNotNone(self.device_id)
-        self.cumulocity_api.delete_managed_object(self.device_id)
-        log.info(f"Removed the test device with id {self.device_id}")
+        self.assertIsNotNone(device_id)
+        self.cumulocity_api.delete_managed_object(device_id)
+        log.info(f"Removed the test device with id {device_id}")
         log.info('-' * 100)
 
     def test_get_or_create_device_id_with_missing_id(self):
-        self.device_model = {
-            "type": "Simulator",
-            "label": "Test Simulator #1",
-            "enabled": "true"
-        }
-        log.info(f"Created device model without id: {self.device_model}")
-        log.info("Start testing create device without id definition")
-        self.device_id = get_or_create_device_id(device_model=self.device_model)
+        device_id = Utils.create_device(self.device_model_no_id)
         # null device_id will fail the test
-        self.assertIsNone(self.device_id)
+        self.assertIsNone(device_id)
         log.info('-' * 100)
 
     def test_get_or_create_device_id_with_missing_label(self):
-        self.device_model = {
-            "type": "Simulator",
-            "id": "sim_001_test",
-            "enabled": "true"
-        }
-        log.info(f"Created device model without label: {self.device_model}")
-        log.info("Start testing create device without label")
-        self.device_id = get_or_create_device_id(device_model=self.device_model)
+        device_id = Utils.create_device(self.device_model_no_label)
         # null device_id will fail the test
-        self.assertIsNone(self.device_id)
+        self.assertIsNone(device_id)
         log.info('-' * 100)
 
     @patch('logging.Logger.error')  # patch to hide the log.error method
     def test_load_json_file(self, mock_error):
         log.info("Start testing load json file")
-        self.model = load("simulators/main/simulator.json") # Load model for unittest CLI
-        if not self.model:
-            self.model = load("../simulators/main/simulator.json") # Load model for unittest on IDE
-        self.assertIsNotNone(self.model)
+        model = load("simulators/main/simulator.json") # Load model for unittest CLI
+        if not model:
+            model = load("../simulators/main/simulator.json") # Load model for unittest on IDE
+        self.assertIsNotNone(model)
         log.info('-' * 100)
 
     def test_create_and_activate_oee_profile(self):
-        self.device_model = {
-            "type": "Simulator",
-            "id": "sim_001_test",
-            "label": "Test Simulator #1",
-            "enabled": "true"
-        }
-        self.device_id = get_or_create_device_id(device_model=self.device_model)
-        self.device_profile_info = self.oee_api.create_and_activate_profile(external_id=self.device_model.get('id'))
-        # null device_id will fail the test
-        self.assertIsNotNone(self.device_profile_info)
-        self.cumulocity_api.delete_managed_object(self.device_profile_info.get('id'))
-        log.info(f"Removed the test oee profile with id {self.device_profile_info.get('id')}")
-        self.cumulocity_api.delete_managed_object(self.device_id)
-        log.info(f"Removed the test device with id {self.device_id}")
+        device_id = Utils.create_device(device_model=self.device_model)
+        device_profile_info = self.oee_api.create_and_activate_profile(external_id=self.device_model.get('id'))
+        # null device_profile_info will fail the test
+        self.assertIsNotNone(device_profile_info)
+        self.cumulocity_api.delete_managed_object(device_profile_info.get('id'))
+        log.info(f"Removed the test oee profile with id {device_profile_info.get('id')}")
+        self.cumulocity_api.delete_managed_object(device_id)
+        log.info(f"Removed the test device with id {device_id}")
         log.info('-' * 100)
+
+class Utils:
+    @staticmethod
+    def create_device(device_model):
+        log.info(f"Created device model: {device_model}")
+        log.info("Start testing create device and adding external id")
+        device_id = get_or_create_device_id(device_model=device_model)
+        return device_id
+
 
 
 if __name__ == '__main__':
