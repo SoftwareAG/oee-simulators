80f6c42e409c3c1d58e3cd9e76f6904b3912c565
Nam Nhat Pham <naph@softwareag.com>
2023-03-21 11:19:31 +0100

    COE-869: remove source directory to extras

--- config/root.py
+++ config/root.py
@@ -4,7 +4,5 @@ import os,sys
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 # Make a path to simulators/main and .../extras (source directory)
 SOURCE_DIR = os.path.join(ROOT_DIR, 'simulators', 'main')
-SOURCE_DIR_2 = os.path.join(ROOT_DIR, 'simulators', 'extras')
 # Add the source directory to the module search path
 sys.path.insert(0, SOURCE_DIR)
-sys.path.insert(0, SOURCE_DIR_2)
