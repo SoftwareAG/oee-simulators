803978530a0f2864d32c6fc0181bbf8b48aad710
Nam Nhat Pham <naph@softwareag.com>
2023-03-21 09:56:21 +0100

    COE-869: idea to test export script

--- test/export_import_test.py
+++ test/export_import_test.py
@@ -1,4 +1,19 @@
 import unittest, logging, os
 import config.root # Set source directory
+
 from unittest.mock import patch
-from simulators.extras.Environment import
+from subprocess import call
+
+class Test(unittest.TestCase):
+    def test_export_profile_data(self):
+        # Run the ExportProfileData.py script
+        call(["python", "../simulators/extras/ExportProfileData.py"])
+
+        # Check if the export_data folder is created
+        self.assertTrue(os.path.exists("export_data"), msg="export_data folder not found")
+
+        # Remove the export_data folder
+        os.rmdir("export_data")
+
+if __name__ == '__main__':
+    unittest.main()
