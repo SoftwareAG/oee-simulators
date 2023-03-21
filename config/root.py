cf5a23469d08f25522479c275a9fe21f147db42d
Nam Nhat Pham <naph@softwareag.com>
2023-03-21 09:49:57 +0100

    COE-869: Add extras to source directory

--- config/root.py
+++ config/root.py
@@ -2,7 +2,9 @@ import os,sys
 
 # Get the path of the parent directory of the current file (i.e., the root folder)
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-# Make a path to simulators/main (source directory)
+# Make a path to simulators/main and .../extras (source directory)
 SOURCE_DIR = os.path.join(ROOT_DIR, 'simulators', 'main')
+SOURCE_DIR_2 = os.path.join(ROOT_DIR, 'simulators', 'extras')
 # Add the source directory to the module search path
 sys.path.insert(0, SOURCE_DIR)
+sys.path.insert(0, SOURCE_DIR_2)
