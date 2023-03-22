953891c6f7393feac54968e737840019b739eb4d
Nam Nhat Pham <naph@softwareag.com>
2023-03-22 10:35:40 +0100

    COE-869: add try and finally to make sure all data files are deleted, change test logic to check if sim_001_profile.json exist and has contents

--- test/export_import_test.py
+++ test/export_import_test.py
@@ -3,6 +3,7 @@ import config.root # Set source directory
 
 from unittest.mock import patch
 from subprocess import call
+from simulators.main.simulator import load
 
 log = logging.getLogger("Test Import Export")
 logging.basicConfig(format='%(asctime)s %(name)s:%(message)s', level=logging.DEBUG)
@@ -18,31 +19,40 @@ class Test(unittest.TestCase):
         except:
             os.chdir("simulators/extras") # Command line
 
-        # Run the ExportProfileData.py script
-        call(["python", "ExportProfileData.py"])
+        try:
+            # Run the ExportProfileData.py script
+            call(["python", "ExportProfileData.py"])
 
-        # Check if the export_data folder is created
-        self.assertTrue(os.path.exists("export_data"), msg="export_data folder not found")
+            # Check if the sim_001_profile.json is created
+            profile_path = os.path.join("export_data", "sim_001_profile.json")
+            self.assertTrue(os.path.exists(profile_path), msg="sim_001_profile.json not found")
 
-        # Run the ImportData.py script and get the exit code
-        exit_code = call(["python", "ImportData.py"])
+            # Open the JSON file and load its contents
+            data = load(profile_path)
+            # Check if the data file is empty
+            self.assertNotEqual(len(data),0, msg="No content in sim_001_profile.json file")
 
-        # Check if the exit code is 0
-        self.assertEqual(exit_code, 0, msg="ImportData.py script failed to run")
 
-        # Iterate over all files and subdirectories in dir_path
-        for filename in os.listdir("export_data"):
-            # Create the full file path by joining the directory and filename
-            file_path = os.path.join("export_data", filename)
+            # Run the ImportData.py script and get the exit code
+            exit_code = call(["python", "ImportData.py"])
 
-            # Check if the file_path is a file or directory and remove it accordingly
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
+            # Check if the exit code is 0
+            self.assertEqual(exit_code, 0, msg="ImportData.py script failed to run")
 
-        # Change back to the original working directory
-        os.chdir(current_dir)
+        finally:
+            # Iterate over all files and subdirectories in dir_path
+            for filename in os.listdir("export_data"):
+                # Create the full file path by joining the directory and filename
+                file_path = os.path.join("export_data", filename)
+
+                # Check if the file_path is a file or directory and remove it accordingly
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+
+            # Change back to the original working directory
+            os.chdir(current_dir)
 
 if __name__ == '__main__':
     unittest.main()
