da3f978a93e41292c57353fd9a91db188a6beb15
Nam Nhat Pham <naph@softwareag.com>
2023-03-29 09:41:45 +0200

    COE-893: remove test case for testing new implement

--- test/simulators_test.py
+++ test/simulators_test.py
@@ -144,13 +144,6 @@ class Test(unittest.TestCase):
                 log.info(f"Deleted shiftplan {shiftplan.locationId}")
         log.info('-' * 100)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world!'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
 class Utils:
     def __init__(self):
         self.shiftplans = None
